7358e83a8883c337f02d36935c2169322c1ab137
makro89 <mgalic99@gmail.com>
2023-02-14 19:23:14 +0100

    promjene

---
+++ Leksicka struktura/moj.js
@@ -0,0 +1,19 @@
+var firstName, lastName, price, discount, fullPrice;
+firstName = "John";
+lastName = "Doe";
+price = 19.9;
+discount = 0.1;
+
+var x = 3;
+x = 1;
+if (x == 1) {
+  x = 2;
+}
+
+var a = true;
+if (a) {
+  var z = 44;
+  return 0;
+}
+
+fullPrice = (price * 100) / discount;
