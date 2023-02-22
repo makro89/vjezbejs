cdaf991b81b516ecab43513167410251fb112d13
makro89 <mgalic99@gmail.com>
2023-02-22 19:19:51 +0100

    promjena

---
+++ funkcije/script3.js
@@ -0,0 +1,24 @@
+/*var dog = {};
+dog.breed = "Golden retriever";
+dog["other breed"] = "Chiuahua";
+
+console.log(dog);
+
+
+var osoba = {};
+osoba.ime = "Marko";
+osoba.prezime = "Galić";
+osoba.godine = "33";
+osoba.brojCipela = "46";
+*/
+var osoba = {
+    ime : "ime",
+    prezime : "prezime",
+    brojCipela : 42,
+};
+
+if(osoba.brojCipela > 39) {
+    console.log("Imamo na stanju");
+}
+
+
