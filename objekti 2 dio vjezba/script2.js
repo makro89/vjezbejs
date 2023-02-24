d852a682a7195f0aa9c98794e9c550713c3eaeff
makro89 <mgalic99@gmail.com>
2023-02-24 19:20:47 +0100

    promjene

---
+++ objekti 2 dio vjezba/script2.js
@@ -0,0 +1,11 @@
+var d = new Date();
+console.log(d);
+var d1 = new Date(2018, 11, 24, 10, 33, 30, 0);
+console.log(d1);
+var d2 = new Date("october 13, 2014 11:13:00");
+console.log(d2);
+var d3 = new Date(-100000000000);
+console.log(d3);
+
+Math.round(4.4);
+console.log()
