e1bf000e2b1f05c3270b6eb038a72829ae79db75
makro89 <mgalic99@gmail.com>
2023-03-02 19:18:00 +0100

    promjene

---
+++ vjezba/novavjezba.js
@@ -0,0 +1,5 @@
+// console.log(`Sirina: ` + window.innerWidth);
+// console.log(`Visina: ` + window.innerHeight);
+
+console.log(window.location.hostname);
+window.location = `http://www.google.com`;
---
+++ zadaća/script1.js
@@ -0,0 +1,9 @@
+let print " "
+for(let i = 1; i <= 10; i++) {
+    for (let j = 1; j <= 10; j++)
+{
+{print +=(j + "*" + i + "=" + (j * i) + "\m");
+}
+print += "------" + "\m";
+}
+console.log("Tablica množenja: " + "\m" + print);
