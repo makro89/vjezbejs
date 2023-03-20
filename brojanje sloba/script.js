5b04aa54908707b83f2ab7b85605f3c9869622e5
makro89 <mgalic99@gmail.com>
2023-03-20 18:56:34 +0100

    promjen

---
+++ brojanje sloba/script.js
@@ -0,0 +1,15 @@
+function sum(a, b) {
+    return a + b;
+}
+var mojaRecenica = "Moja recenica sadrzi 3 slova e";
+function brojSlovaE() {
+    let retVal = 0;
+    let e = "e";
+    for (let i = 0; < mojaRecenica.length; i++) {
+        let slovo = mojaRecenica[i];
+        if (slovo === e) {
+            retVal++;
+        }
+    }
+    return retVal;
+}
